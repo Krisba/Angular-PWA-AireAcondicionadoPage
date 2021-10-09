38ee615c289c26539e6bb6cb417618fa567bd205
Christian <krisba95@gmail.com>
2021-10-09 02:16:34 -0300

    navbar completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'home', component: HomeComponent },
+
+  {path : '' , pathMatch : 'full', redirectTo : 'home'},
+  {path: '***', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
