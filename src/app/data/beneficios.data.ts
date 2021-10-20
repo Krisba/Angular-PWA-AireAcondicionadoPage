6eea9ec5795723b83e76aaec299821a0d6d843cd
Christian <krisba95@gmail.com>
2021-10-19 22:07:02 -0300

    arreglos responsive, iconos gif

--- src/app/data/beneficios.data.ts
+++ src/app/data/beneficios.data.ts
@@ -1,37 +1,37 @@
 export const BENEFICIOS  = [
 
     {
-        video: '../../assets/videos/calidad.mp4',
+        gif: '../../assets/images/calidad.gif',
         titulo: 'CALIDAD',
         descripcion: 'Ofrecemos la mejor calidad de servicio al mejor precio. Cumplimos siempre con lo que prometemos.',
     },
 
     {
-        video: '../../assets/videos/garantia.mp4',
+        gif: '../../assets/images/garantia.gif',
         titulo: 'GARANTÍA',
         descripcion: 'Sentí la tranquilidad de contar con un servicio que te respalda en cualquier momento.',
     },
 
     {
-        video: '../../assets/videos/precio.mp4',
+        gif: '../../assets/images/precio.gif',
         titulo: 'PRECIO',
         descripcion: 'El precio que se acuerda en el presupuesto es el precio a pagar. Sin gastos extra.',
     },
 
     {
-        video: '../../assets/videos/satisfaccion.mp4',
+        gif: '../../assets/images/satisfaccion.gif',
         titulo: 'SATISFACCIÓN',
         descripcion: 'Contamos con un 97% de clientes satisfechos con todos nuestros servicios.',
     },
 
     {
-        video: '../../assets/videos/atencion.mp4',
+        gif: '../../assets/images/atencion.gif',
         titulo: 'ATENCIÓN',
         descripcion: 'Contamos con personal que atiende tus problemas las 24hs los 365 dias del año.',
     },
 
     {
-        video: '../../assets/videos/profesionales.mp4',
+        gif: '../../assets/images/profesionales.gif',
         titulo: 'PROFESIONALES',
         descripcion: 'Tenemos cientos de profesionales homologados en todo el territorio provincial.',
     },
